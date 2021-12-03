4af252e7be25ad40c929b7d68cb2ce3b48989644
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-12-03 09:24:23 -0600

    enteros largos

---
+++ 20211203/11-enteros.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+entero_maximo_32bit = 2**32
+entero_maximo_64bit = 2**64
+entero = 18446744073709551616000
+
+print("El entero maximo de 32 bits es", entero_maximo_32bit)
+print("El entero maximo de 64 bits es", entero_maximo_64bit)
+print("El entero maximo de xx bits es", entero)
+print("El entero maximo de xx bits es", entero+entero)
