b4e84cf9b4e359a576e55d40e616d6793edf7f47
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-12-06 09:04:30 -0600

    casting

---
+++ 20211206/03-casting.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+entero = 28
+flotante = float(entero)
+
+print(entero, "es", type(entero) )   # 28 es <class 'int'>
+print(flotante, "es",  type(flotante) )
+
+
+flotante = 9.5
+entero = int(flotante)
+
+print(entero, "es", type(entero) )
+print(flotante, "es",  type(flotante) )
