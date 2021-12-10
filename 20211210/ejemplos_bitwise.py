e5e191ce33c5bf67c764a074e391a2e10fd23d85
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-12-10 09:05:06 -0600

    terminando ejemplo bitwise

--- 20211210/ejemplos_bitwise.py
+++ 20211210/ejemplos_bitwise.py
@@ -7,4 +7,21 @@
 
 x = 22
 
+print("x      =        {:>6b}".format(x))
+el_and = x & 4
+print("x & 4  = {:>3d}  = {:>6b}".format(x & 4, el_and))
+print("x | 1  = {:>3d}  = {:>6b}".format(x | 1, x | 1))
+print("x ^ 4  = {:>3d}  = {:>6b}".format(x ^ 4, x ^ 4))
+print("~x     = {:>3d}  = {:>6b}".format(~x, ~x))
+# x =   22    =   0...0000010110
+# ~x          =   1...1111101001   
+
+#  complemento a 2 -> invertir los valores y sumar 1
+# y =   23    =   0...0000010111
+#                 1...1111101000 invertir los valores
+# y =  -23    =   1...1111101001 sumar 1
+print("x << 1 = {:>3d}  = {:>6b}".format(x << 1, x << 1))
+print("x >> 2 = {:>3d}  = {:>6b}".format(x >> 2, x >> 2))
+
+
 
