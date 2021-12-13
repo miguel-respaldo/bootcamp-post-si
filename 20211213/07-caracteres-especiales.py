691f2a0a75f08e31086e2ef6df80cf5c87913922
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-12-13 10:05:28 -0600

    caracteres especiales

---
+++ 20211213/07-caracteres-especiales.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+archivo = "C:\\Mis\nDocumentos\\\'mi archivo.txt\'"
+print(archivo)
+
+print("***Hola Mundo\r1. ")
+print("123456789\rabcd")
+print("123456789\tabcd")
+print("123456789\b\babcd")
+print("123456789\fabcd")
