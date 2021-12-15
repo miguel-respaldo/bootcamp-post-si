45f64ab459150ca3674f90ee37435957bb9b4c9f
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-12-15 09:34:35 -0600

    funcion matematica

---
+++ 20211215/04-raiz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import math
+
+# sqrt = square root -> raiz cuadrada
+print(math.sqrt(9)) # imprime 3.0
+
+print(math.cos(0)) # imprime 1.0
+print(math.sin(math.pi)) # imprime ~0.0
+
