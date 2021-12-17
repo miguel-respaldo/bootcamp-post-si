b90ef2bb42975bf67539c179f558640854306e4e
enya1312 <enya.gomez@iteso.mx>
2021-12-17 08:57:59 -0600

    parte enya

--- 20211217/angel.ramos_y_enya.gomez/chicharronera.py
+++ 20211217/angel.ramos_y_enya.gomez/chicharronera.py
@@ -4,17 +4,10 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-"""
-Ejemplo de un modulo
-"""
 
-def main():
-    """
-    Comentario de la función
-    """
-    print("Hola Mundo")
+c= float(input('Ingrese el valor de c:  '))
+a= float(input('Ingresa el valor de a:  '))
+b= float(input('Ingresa el valor de b:  '))
 
-
-if __name__ == "__main__":
-    main()
-
+x1=(-b+(((b**2)-(4*a*c))**0.5))/(2*a)
+x2=(-b-(((b**2)-(4*a*c))**0.5))/(2*a)
