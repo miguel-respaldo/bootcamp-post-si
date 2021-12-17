41d0b98f6f040a74497e42aaa0d1c5837e2afe16
Raul Suarez <raul.suarez@iteso.mx>
2021-12-17 08:40:16 -0600

    Compartiendo

---
+++ 20211217/dafne.hernandez_y_raul.surez/formula_general.py
@@ -0,0 +1,12 @@
+import math
+
+
+def formula_general(a, b, c):
+    pass
+
+
+print("---------- CALCULADORA DE FORMULA GENERAL ----------")
+a = float(input("Ingresa el valor de a: "))
+b = float(input("Ingresa el valor de b: "))
+c = float(input("Ingresa el valor de c: "))
+d = float(input("Ingresa el resultado de la ecuacion: "))
