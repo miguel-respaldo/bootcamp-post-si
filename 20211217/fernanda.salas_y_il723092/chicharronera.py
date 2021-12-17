83229cfb2604022b033a394e0d0050159587735c
Javier de Alba <il723092@iteso.mx>
2021-12-17 09:07:52 -0600

    Terminar el codigo

--- 20211217/fernanda.salas_y_il723092/chicharronera.py
+++ 20211217/fernanda.salas_y_il723092/chicharronera.py
@@ -4,14 +4,28 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-from math import sqrt 
+#from math import sqrt 
+import math
 
-a = (int(input("Ingrese el valor de a: "))
-b = (int(input("Ingrese el valor de b: "))
-c = (int(input("Ingrese el valor de c: "))   
+a = (int(input("Ingrese el valor de a: ")))
+b = (int(input("Ingrese el valor de b: ")))
+c = (int(input("Ingrese el valor de c: ")) )  
 
 x1 = 0
 x2 = 0
 
+if ((b**2)-4*a*c) < 0:
+    print("Ecuación con números complejos")
+    c1 = complex(-b/(2*a),math.sqrt(-1*((b**2)-4*a*c))/(2*a))
+    c2 = complex(-b/(2*a),-1*math.sqrt(-1*((b**2)-4*a*c))/(2*a))
+    
+    print("El resultado complejo en x1 es:",c1.real,"+",c1.imag,"j")
+    print("El resultado complejo en x2 es:",c2.real,"-",-1*c2.imag,"j")
 
+else:
+    x1 = (-b + math.sqrt((b**2)-4*a*c))/(2*a)
+    x2 = (-b - math.sqrt((b**2)-4*a*c))/(2*a)
+
+    print("El resultado en x1 es:",x1)
+    print("El resultado en x2 es:",x2)
 
