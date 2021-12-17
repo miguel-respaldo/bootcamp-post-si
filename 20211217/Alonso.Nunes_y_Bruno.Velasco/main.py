87e14e9bd9aaefd7ddbfecc7d9da79f636585ae7
Bruo Giancarlo Velasco Bravo <bruno.velasco@iteso.mx>
2021-12-17 08:59:21 -0600

    con if, falta terminar else

--- 20211217/Alonso.Nunes_y_Bruno.Velasco/main.py
+++ 20211217/Alonso.Nunes_y_Bruno.Velasco/main.py
@@ -4,11 +4,21 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-"""
+
 from math import sqrt
+
 
 a = input("Introduzca valor para A: ")
 b = input("Introduzca valor para B: ")
 c = input("Introduzca volor para C: ")
 
-x = (b ** 2 - 4 * a * c) 
+b4ac = (b ** 2 - 4 * a * c)
+
+if (b4ac > 0):
+    sqr = sqrt(b4ac)
+    z1 = (-b + sqr) / (2 * a)
+    z2 = (-b - sqr)
+else:
+    b4ac = b4ac * -1
+    z1 = (-b + complex(0, sqr)) / (2 * a)
+    #z2 = (-b - )
