4040558d31fd1a7ecb34f2692dccc816c14baf05
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2021-12-17 09:49:07 -0600

    cambiosfinales

--- 20211217/dafne.hernandez_y_raul.surez/formula_general.py
+++ 20211217/dafne.hernandez_y_raul.surez/formula_general.py
@@ -1,34 +1,31 @@
 import math
 
-
-def formula_general(a, b, c, d):
+def formula_general(a, b, c):
     if a == 0:
         print("El valor de a no puede ser igual a 0")
         raise ValueError("A no puede ser igual a 0")
-
-    formula = abs ( b ** 2 - 4 * a * c)  # si el numero resultante es negativo no hay solucione reales
-
-    if d == 0: #si es igual a 0 solo hay una solucion
-        resultado = (-b + math.sqrt(formula)) / 2 * a
-        print("La raiz unica es {:.3f}".format(resultado))
-
-    elif d > 0: #si es mayor a 0 entonces hay dos soluciones
-        resultado = (-b + math.sqrt(formula)) / (2 * a)
-        resultado_2 = (-b + math.sqrt(formula)) / (2 * a)
-        print("La raiz es {:.3f}".format(resultado))
-        print("La raiz es {:.3f}".format(resultado_2))
     else:
-        x1 = (-b + math.sqrt(formula)) / (2 * a)
-        x2 = (-b - math.sqrt(formula)) / (2 * a)
-        print("La raiz real x1 es {:.3f}+i".format(x1))
-        print("La raiz real x2 es {:.3f}-i".format(x2))
+        formula = abs(b ** 2 - 4 * a * c) # si el numero resultante es negativo
+        #entonces es complejo, es el discriminante
+        if formula >= 0: # es positivo, dos soluciones
+           if formula == 0: #si es igual a 0 solo hay una solucion
+               resultado = (-b + math.sqrt(formula)) / 2 * a
+               print("La raiz unica es {:.3f}".format(resultado))
+           else:
+            	x1 = (-b + math.sqrt(formula)) / (2 * a)
+            	x2 = (-b - math.sqrt(formula)) / (2 * a)
+            	print("La raíz real x1 es {:.3f}".format(x1))
+            	print("La raíz real x2 es {:.3f}".format(x2))
+        else:
+              formula = abs(formula)
+              parteReal = -b / (2 * a)
+              parteImaginaria = math.sqrt(siReal) / (2 * a)
+              print("La raíz compleja x1 es {:.3f} + {:.3f}i".format(parteReal, parteImaginaria))
+              print("La raíz compleja x2 es {:.3f} - {:.3f}i".format(parteReal, parteImaginaria))
 
-
-    
 print("---------- CALCULADORA DE FORMULA GENERAL ----------")
 a = float(input("Ingresa el valor de a: "))
 b = float(input("Ingresa el valor de b: "))
 c = float(input("Ingresa el valor de c: "))
-d = float(input("Ingresa el resultado de la ecuacion: "))
 
-formula_general(a,b,c,d)
+formula_general(a,b,c)
