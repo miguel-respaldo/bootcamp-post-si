9531b2f27f40d1a7d0f906fa3cb24c8b223ba4db
Angel Alexis Ramos Castellanos <angel.ramos.castellanos@gmail.com>
2021-12-17 09:27:15 -0600

    comentarios

--- 20211217/angel.ramos_y_enya.gomez/chicharronera.py
+++ 20211217/angel.ramos_y_enya.gomez/chicharronera.py
@@ -5,19 +5,19 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-
-a= float(input('Ingresa el valor de a:  '))
+#solicitar datos de entrada a, b, c de la ecuacion cuadratica
+a= float(input('Ingresa el valor de a:  ')) 
 b= float(input('Ingresa el valor de b:  '))
 c= float(input('Ingrese el valor de c:  '))
 
-x1=(-b+(((b**2)-(4*a*c))**0.5))/(2*a)
-x2=(-b-(((b**2)-(4*a*c))**0.5))/(2*a)
+x1=(-b+(((b**2)-(4*a*c))**0.5))/(2*a ) #calculo de x1
+x2=(-b-(((b**2)-(4*a*c))**0.5))/(2*a) #calculo de x2
 
 print('el valor de x1 es: ')
-print(x1)
+print(x1) #imprime x1
 
 print('el valor de x2 es: ')
-print(x2)
+print(x2) #imprime x2
 
 if a==0:
-    print('no puede ser 0')
+    print('no puede ser 0') # si a=0, el valor es infinito
