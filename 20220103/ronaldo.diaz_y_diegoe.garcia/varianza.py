d7143e6d618b91d9672fd4f626e1fdb3d5aa01bf
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-01-03 12:38:49 -0600

    tenemos una duda

--- 20220103/ronaldo.diaz_y_diegoe.garcia/varianza.py
+++ 20220103/ronaldo.diaz_y_diegoe.garcia/varianza.py
@@ -10,22 +10,26 @@ Ejemplo de un modulo
 
 def main():
 
-    numeros=[]          # se declara una lista vacia
-    varianza=0            # se declara variable de varianza
-    sumatoria=0           # sumatoria auxiliar
+    numeros=[]              # se declara una lista vacia
+    varianza=0              # se declara variable de varianza
+#    sumatoria=0             # sumatoria auxiliar
+#    n=0                     # cantidad de numeros a ingresar
 
-    print("Este programa obtiene la varianza de 7 numeros")
+    print("Este programa obtiene la varianza")
+    n=int(input("Ingresa la cantidad de numeros para obtener la varianza: "))
 
-    for x in range(7):  # se hace un for para obtener los 7 valores
-        val=int(input("Ingrese el numero:"))
-        numeros.append(val)
+    for x in range(n):  # se hace un for para obtener los 7 valores
+        dato=int(input("Ingrese el numero: "))
+        numeros.append(dato) #agrega los elementos a la lista
         
-    n = len(numeros) #se obtiene el tamano de la lista
-    
-    sumatoria = sum(numeros)
-        
-    print(sumatoria)
-        
+    mean = sum(numeros) / n
+
+    #for x in range(n):
+       #suma = (numeros[x]-mean)**2 
+
+    varianza = sum((x-mean)**2 for x in numeros) / (n-1)
+    #varianza = (1/(n-1)) * suma
+    print("\nLa varianza es:",varianza)
 
 if __name__ == "__main__":
     main()
