332664077f6c1a08d05afc73e36a1f92efd72850
Bruo Giancarlo Velasco Bravo <bruno.velasco@iteso.mx>
2022-01-03 10:33:08 -0600

    varianza, cantidad de entradas variables

--- 20220103/Pedro.Sanchez_y_Bruno.Velasco/main.py
+++ 20220103/Pedro.Sanchez_y_Bruno.Velasco/main.py
@@ -5,17 +5,31 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 """
-Ejemplo de un modulo
+Varianza muestreal
 """
 
 def main():
-    n1=0
-    n2=0
-    n3=0
-    n4=0
-    n5=0
-    n6=0
-    n7=0
+    
+    x = []
+    sumatoria = 0.0
+    suma = 0.0
+    promedio = 0.0
+
+    n = int(input("Cantidad de muestras: "))
+    for i in range(n):
+        entrada = float(input("Ingrese la entrada {}: ".format(i+1)))
+        x.append(entrada)
+
+    for i in range(n):
+        suma += x[i]
+    promedio = suma / n
+    
+    for i in range(n):
+        #print((x[i] - promedio))
+        sumatoria += (x[i] - promedio) ** 2
+
+    print(sumatoria/n)
+
 
 
 if __name__ == "__main__":
