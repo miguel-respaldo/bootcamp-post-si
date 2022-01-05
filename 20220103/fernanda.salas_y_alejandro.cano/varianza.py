0782a677dcfe9fef9ba926ad72d3d8b634b744a4
fernandasalas <fernanda.salas@iteso.mx>
2022-01-04 22:17:47 -0600

    Practica completa

--- 20220103/fernanda.salas_y_alejandro.cano/varianza.py
+++ 20220103/fernanda.salas_y_alejandro.cano/varianza.py
@@ -1,14 +1,28 @@
-import statistics
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-a = float(input("Ingrese el primer numero: "))
-b = float(input("El segundo: "))
-c = float(input("El tercero: "))
-d = float(input("EL cuarto: "))
-e = float(input("El quinto: "))
-f = float(input("EL sexto: "))
-g = float(input("El septimo: "))
+lista = [] #creamos una lista vacia
 
-data = [a, b, c, d, e, f, g]
-#def statistics.variance(data)
-print ("VARIANZA: ", statistics.variance(data))
+sumatoria = 0 #inicializamos en cero sumatoria
+
+for i in range(7):
+    dato = int(input("Ingrese un valor: "))
+    lista.append(dato) #agregamos los datos ingresados a nuestra lista
+    sumatoria += dato #se realiza la sumatoria de los datos
+
+media = sumatoria / 7 #calculamos la media
+
+desviacion = 0 #inicializamos nuestra variable en 0
+
+for j in range(7):
+    desviacion += (lista[j]-media)**2 #calculamos la desviacion
+
+
+varianza = desviacion / 7
+
+print("La varianza es: ", varianza)
 
