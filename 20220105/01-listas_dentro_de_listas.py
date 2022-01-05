31aed36ffa7346cf5bfa651a574d8f8580700561
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-01-05 08:38:28 -0600

    lista dentro de listas

---
+++ 20220105/01-listas_dentro_de_listas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+matriz = [[1,2],[3,4]]
+
+# 0 [ 1, 2 ]  
+# 1 [ 3, 4 ]
+
+print(matriz[0][0]) # 1
+print(matriz[0][1]) # 2
+print(matriz[1][0]) # 3
+print(matriz[1][1]) # 4
