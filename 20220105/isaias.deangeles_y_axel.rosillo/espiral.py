34e39ba8d21a2afddfbf1352f9dce5766d16e167
Axel Rosillo Enciso <axel.rosillo@iteso.mx>
2022-01-06 12:00:07 -0600

    Practica espiral avanzada

--- 20220105/isaias.deangeles_y_axel.rosillo/espiral.py
+++ 20220105/isaias.deangeles_y_axel.rosillo/espiral.py
@@ -9,16 +9,31 @@ Practica: Matriz espiral
 """
 
 def main():
-    n = int (input ( "Ingrese la dimension de la matriz" ) )
+    n = int (input ( "Ingrese la dimension de la matriz: " ) )
     
-    matriz = [[0 for i in range (n)] for j in range(n)]
+    matriz = [[0 for i in range(n)] for j in range(n)]
+    nivel = 0
+    coordenada = 0
 
-    nivel, posicion = 0, 0
+    while coordenada < n * n:
+        
+        num = n - (nivel * 2) - 1
+        temp = coordenada
 
-    while posicion < n * n
-        numero = n - ( nivel * 2) - 1
+        for i in range(num):
+            temp += 1
+            matriz[nivel][i+nivel] = temp
+            matriz[nivel+i][n-nivel-1] = temp + num
+            matriz[n-nivel-1][n-nivel-i-1] = temp + num * 2
+            matriz[n-nivel-i-1][nivel] = temp + num * 3
+         
+        nivel += 1
+        coordenada += num * 4
 
-
+    for i in matriz:
+        for j in i:
+            print('%3s' % j, end='')
+        print("")
 
 
 if __name__ == "__main__":
