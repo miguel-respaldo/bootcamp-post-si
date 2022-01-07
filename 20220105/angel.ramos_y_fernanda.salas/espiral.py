b2a439ac0937e42e88f0f3f31b716530828c3b56
angel <angel.ramos.castellanos@gmail.com>
2022-01-07 02:48:50 -0600

    ya quedó

--- 20220105/angel.ramos_y_fernanda.salas/espiral.py
+++ 20220105/angel.ramos_y_fernanda.salas/espiral.py
@@ -8,24 +8,52 @@
 #matriz = [[1,2],[3,4]]
 matriz = []
 #matriz = list()
-n = eval(input("ingrese tamaño matriz"))
+n = eval(input("ingrese tamaño matriz: "))
 
-for i in range(n): # 1
+
+#crear matriz de n X n de ceros
+
+for i in range(n):
     matriz.append([])
     for j in range(n): 
         matriz[i].append(0)
-        
+
 contador = 0
-for i in range(n):
-    contador = contador + 1
-    matriz[i][j] = contador
+m=n
+# posiciones
+a=0 
+b=n-1
+c=n-1
+e=n-1
 
+while contador!=n**2: #hasta que el contador llegue a n al cuadrado
 
-#print(matriz[0][0]) # 1
-#print(matriz[0][1]) # 2
-#print(matriz[1][0]) # 3
-#print(matriz[1][1]) # 4
+#de izq a derecha
+    for i in range(m):
+        contador = contador + 1
+        matriz[a][i+a] = contador
+    m=m-1
+    a=a+1
 
+#de arriba hacia abajo
+    for i in range(m):
+        contador = contador + 1
+        matriz[a+i][b] = contador
+    b=b-1
+
+#de derecha a izq
+    for i in range(m):
+        contador = contador + 1
+        matriz[c][b-i] = contador
+    m=m-1
+    c=c-1
+
+#de abajo hacia arriba
+    for i in range(m):
+        contador = contador + 1
+        matriz[c-i][a-1] = contador 
+
+#impresión de la matriz
 for i in range(len(matriz)):
     print("[ ", end="")
     for j in range(len(matriz[i])):
