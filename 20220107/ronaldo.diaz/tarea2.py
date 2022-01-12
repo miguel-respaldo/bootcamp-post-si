5f6918a87dda6c6f92340daa8b1c179b4e3de8d8
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-01-12 02:55:13 -0600

    a mimir

--- 20220107/ronaldo.diaz/tarea2.py
+++ 20220107/ronaldo.diaz/tarea2.py
@@ -11,9 +11,11 @@ Tarea 2:
 m: columnas
 n: filas
 """
-import random
-matriz_final = []
-result = int(0)
+import random           # se llama a random para rellenar matrices
+
+matriz_final = []       # se declara la matriz final
+result = int(0)         # se declara una variable auxiliar para el resultado de
+                        # la multiplicacion de elementos entre matrices
 
 print("El siguiente programa realiza multiplicacion de matrices\n")
 n1 = int(input("Ingresa el numero de filas de la primer matriz: "))
@@ -28,36 +30,62 @@ else:
     print("\nVamonos recio")
     llenado = input("Deseas que las matrices se llenen automaticamente con numero aleatorios? (y/n): ")
     
+    # - - - - - LLENADO DE MATRICES - - - - -
+
     if(llenado == 'y'):         # se da la opcion de llenado automatico al usuario
         print("Llenado de matriz automatico random ribunucleico... ")
         matriz1 = [[random.randrange(1,100) for columna in range(m1)] for fila in range(n1)]
         matriz2 = [[random.randrange(1,100) for columna in range(m2)] for fila in range(n2)] 
         
-        print("Matriz 1:")      # imprime el resultado de matriz 1 random
-        for fila in range(n1):
-            print("[", end=" ")
-            for columna in range(m1):
-                print("\t{:d} ".format(matriz1[fila][columna]), end="\t")
-            print("]")
-        
-        print("\nMatriz 2:")    # imprime el resultado de matriz 2 random
-        for fila in range(n2):
-            print("[", end=" ")
-            for columna in range(m2):
-                print("\t{:d} ".format(matriz2[fila][columna]), end="\t")
-            print("]")
-
     else:                       # ingresa los datos de la matriz manualmente:
+        matriz1 = []
+        matriz2 = []
         print("No lo hagas, ingresa los datos random bro")
-         
-    # Multiplicacion
+        
+        # Rellena la primer matriz
+        print("\nRellena la primer matriz: ")
+        for fila in range(n1):
+            matriz1.append([])
+            for columna in range(m1):
+                print("Ingresa el dato de la fila",fila,"columna",columna, end="")
+                matriz1[fila].append(int(input(": ")))
+        #Rellena la segunda matriz
+        print("\nRellena la segunda matriz: ")
+        for fila in range(n2):
+            matriz2.append([])
+            for columna in range(m2):
+                print("Ingresa el dato de la fila",fila,"columna",columna, end="")
+                matriz2[fila].append(int(input(": ")))
+    
+
+    # - - - - - IMPRIME MATRICES - - - - -
+    
+    print("Matriz 1:")      # imprime el resultado de matriz 1 random
+    for fila in range(n1):
+        print("[", end=" ")
+        for columna in range(m1):
+            print("\t{:d} ".format(matriz1[fila][columna]), end="\t")
+        print("]")
+
+    print("\nMatriz 2:")    # imprime el resultado de matriz 2 random
+    for fila in range(n2):
+        print("[", end=" ")
+        for columna in range(m2):
+            print("\t{:d} ".format(matriz2[fila][columna]), end="\t")
+        print("]")
+
+
+    # - - - - - MULTIPLICACION DE MATRIZ FINAL - - - - -
     for fila in range(n1):
         matriz_final.append([])
         for columna in range(m2):
-            for res in range(n2):   # se intera el numero en comun
+            for res in range(m1):   # se intera el numero en comun m1 o n2
+
+                # se multiplica elemento por elemento y se suma el resultado de n1 x m2
                 result += matriz1[fila][res] * matriz2[res][columna]
-            matriz_final[fila].append(result)
-        
+            matriz_final[fila].append(result)   # se rellena cada elemnto con el resultado de la multiplicacion
+            result = 0                          # se reinicia el resultado
+
     print("\nMatriz Resultante de la Multiplicacion:")
     for fila in range(n1):
         print("[", end=" ")
