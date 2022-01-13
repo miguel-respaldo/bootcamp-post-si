6d5164066e86324ef4b8a88b7d63939aaa4795bb
Axel Rosillo Enciso <axel.rosillo@iteso.mx>
2022-01-12 20:17:12 -0600

    Practica Ordenamiento avanzada

--- 20220112/jvelazquez.lozano_y_axel.rosillo/burbuja.py
+++ 20220112/jvelazquez.lozano_y_axel.rosillo/burbuja.py
@@ -7,21 +7,42 @@
 """
 Practica: Ordenamiento
 """
+import random
+import time
 
-def burbuja(A):
-    for i in range(1,len(A)):
-        for j in range(0,len(A)-i):
-            if(A[j+1] < A[j]):
-                aux=A[j];
-                A[j]=A[j+1];
-                A[j+1]=aux;
-    print (A)
+def burbuja(ordenar):   # Funcion de ordenamiento burbuja
+    for i in range(1,len(ordenar)): # Ciclo de 1 a la longitud de la lista
+        for j in range(0,len(ordenar)-i):
+            if(ordenar[j+1] < ordenar[j]): # Compara si el siguiente elemento es menor
+                aux=ordenar[j];
+                ordenar[j]=ordenar[j+1];
+                ordenar[j+1]=aux; # Hace el intercambio
+    print (ordenar)
 
 def main():
-    A=[6,5,3,1,8,7,2,4];
-    print(A)
-    burbuja(A);
 
+    lista = random.sample(range(0, 1000), 1000) # Crea una lista de numeros aleatorios
+    print(lista)
+    
+    inicio = time.time() # Inicializa el contador de tiempo
+    burbuja(lista); # Ordena con burbuja
+    fin = time.time() # Guarda el tiempo final
+    tiempo_burbuja = fin-inicio # Calcula el tiempo de ejecucion 
+    
+    inicio = time.time() # Inicializa el contador de tiempo
+    lista.sort() # Ordena con sort
+    #print (lista)
+    fin = time.time()  # Guarda el tiempo final
+    tiempo_sort = fin-inicio  # Calcula el tiempo de ejecucion 
+    
+    print("El algoritmo burbuja tuvo un tiempo de: ")
+    print(tiempo_burbuja)
+    
+    print("La funcion sort tuvo un tiempo de: ")
+    print(tiempo_sort)
+    
+    
+    
 
 if __name__ == "__main__":
     main()
