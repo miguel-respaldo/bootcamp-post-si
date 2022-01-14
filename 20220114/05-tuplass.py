005462e91c619004ee891a93cf4a4096414e2efe
Javier-Vl <jvelazquez.lozano@iteso.mx>
2022-01-14 08:53:52 -0600

    sencillita

---
+++ 20220114/05-tuplass.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+
+tuplaX= (1,2,3)
+tuplaY=(2,3,4)
+
+for x in range(3):
+    print(tuplaX[x]*tuplaY[x])
+    
+# que pasa si "sumo" dos tuplas (t3 = t1 + t2)
+# que pasa si "multiplico" una tupla  ( t3 = 3 * t1)
+
+
