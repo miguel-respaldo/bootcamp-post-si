ec23d39ca96dca88bcbb0bef005cb08f6a50dd3d
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-01-14 08:59:58 -0600

    multiplicación tupla tupla

---
+++ 20220114/06-tupla_por_tupla.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+
+tuplaX= (1,2,3)
+tuplaY=(2,3,4)
+
+tupla = tuplaX * tuplaY
+print(tupla)
+    
+# que pasa si "sumo" dos tuplas (t3 = t1 + t2)
+# que pasa si "multiplico" una tupla  ( t3 = 3 * t1)
+
+
