f901b8ba7f518a1ab24052229824100564aff81c
Gustavo Adolfo Bonilla González <gustavo.bonilla@iteso.mx>
2022-01-17 13:53:03 -0600

    Funciones xD

---
+++ 20220117/david.ramos_y_gustavo.bonilla/factorial.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+    Factorial
+"""
+
+def main():
+    """
+    Ejecución de factorial
+    """
+
+    print("\nHola!!, Este script calcula el facorial de un número.")
+    num = eval(input("\nIntroducir el número del que se descea su factorial: "))
+    print("Hola Mundo")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ 20220117/david.ramos_y_gustavo.bonilla/funciones.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+    Funciones para ejecución de Factorial y de la función de Fibonacci
+"""
+
+def factorial(num):
+    """
+        Función factorial
+    """
+    
+    if(num == 0): return 1
+
+    if(num > 1):
+        result = num * factorial(num-1)
+    else:
+        result = num
+
+    return result
+
+
+def fibonacci(num1, num2, num3, num4):
+    """
+        Función fibonacci
+    """
+    
+    if(num == 0): return 0
+
+    if(num3==1 or num3==2):
+        result = 1
+    
+    if(num3 > 2):
+        fibonacci(num2, num1+num2, num3, num4+1)
+
+
+
+    return result
+
+
+def fibonacci_2(num1):
+    """
+        Función fibonacci
+    """
+    
+    if(num == 0): return 0
+
+    if(num==1 or num==2):
+        result = 1
+    
+    if(num > 2):
+        result = fibonacci_2(num-1)+fibonacci_2(num-2)
+
+    return result
