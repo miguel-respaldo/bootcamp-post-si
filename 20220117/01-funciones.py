73f38d3f8d9d0cb29e8263f26940c30227f886b2
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-01-17 12:27:25 -0600

    01-funciones.py

---
+++ 20220117/01-funciones.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+def mi_funcion(n3,n2,n1):
+    print(f"n3 = {n3}")
+    print(f"n1 = {n1}")
+    print(f"n2 = {n2}")
+
+
+mi_funcion("uno", n1=2, n2="algo")
+
+print("sin salto de linea", end="")
+print("+++")
+
