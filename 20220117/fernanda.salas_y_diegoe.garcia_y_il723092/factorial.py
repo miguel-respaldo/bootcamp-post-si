fbad7c151be4404887b31f2950cb24c24e36612b
Javier de Alba <il723092@iteso.mx>
2022-01-17 13:51:44 -0600

    Finalización

---
+++ 20220117/fernanda.salas_y_diegoe.garcia_y_il723092/factorial.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Ejemplo de un modulo
+"""
+import funciones
+
+def main():
+    """
+    Comentario de la función
+    """
+    n = int(input("Ingresa el tamaño del factorial: "))
+    x = funciones.factorial(n)
+    print(f"El resultado es {x}")
+
+
+if __name__ == "__main__":
+    main()
+
