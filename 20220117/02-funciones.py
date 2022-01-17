413fc1576ef890fb058e12d2394625015268b977
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-01-17 12:33:25 -0600

    mas funciones

---
+++ 20220117/02-funciones.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+def mi_funcion(n3,n2,n1):
+    pass
+
+
+def mi_funcion2():
+    print("hola")
+
+print(mi_funcion("uno", n1=2, n2="algo"))
+print(mi_funcion2())
+
+
