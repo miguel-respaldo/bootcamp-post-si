8926c91bbf210fb71cf36efb8c434719d0f79797
enya1312 <enya.gomez@iteso.mx>
2022-01-19 14:06:47 -0600

    copiar

---
+++ 20220119/il723092_y_enya.gomez/practica.py
@@ -0,0 +1,11 @@
+def main(): 
+    #print("Hola Mundo")
+    
+    f = input("Introduce el nombre del archivo (con extension) a copiar: ")
+    archivo1 = open( f"copia.txt", "w")  
+    archivo2 = open( f+".txt" , "r")
+    archivo1.write(archivo2.read())
+    archivo1.close()
+    archivo2.close()
+	
+
