c462e65ee7712820972cc031dcb6bc6304a980ed
Isayasa663 <isaias.deangeles@iteso.mx>
2022-01-19 14:05:42 -0600

    actualizacion

--- 20220119/Dafne.Hernandez_e_Isaias.deangeles/practica.py
+++ 20220119/Dafne.Hernandez_e_Isaias.deangeles/practica.py
@@ -4,9 +4,15 @@
 import os
 
 
-archivo = imput("Introduce el nombre del archivo que quieras copiar")
+archivo = input("Introduce el nombre del archivo que quieras copiar")
+
 
 if os.path.exists(archivo):
+    contenido = open(archivo, "r")
+    copia = open("copia.txt", "x")
+    copia.write(contenido)
+    copia.close()
+
     os.remove(archivo)
 else:
     print("El archivo no existe")
