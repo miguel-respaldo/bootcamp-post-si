85137404a64e609912149edcff107b4eaa84590f
enya1312 <enya.gomez@iteso.mx>
2022-01-19 14:42:29 -0600

    tarea

--- 20220119/il723092_y_enya.gomez/practica.py
+++ 20220119/il723092_y_enya.gomez/practica.py
@@ -2,7 +2,7 @@ def main():
     #print("Hola Mundo")
     
     f = input("Introduce el nombre del archivo (con extension) a copiar: ")
-    archivo1 = open( f"copia.txt", "w")  
+    archivo1 = open( f'{copia.txt}', 'w')  
     archivo2 = open( f+".txt" , "r")
     archivo1.write(archivo2.read())
     print(archivi2.read())
