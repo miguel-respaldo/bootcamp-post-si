b01bd1402a239ffc936e743926a7b918c1c0c316
enya1312 <enya.gomez@iteso.mx>
2022-01-19 14:30:08 -0600

    tarea

--- 20220119/il723092_y_enya.gomez/practica.py
+++ 20220119/il723092_y_enya.gomez/practica.py
@@ -5,7 +5,8 @@ def main():
     archivo1 = open( f"copia.txt", "w")  
     archivo2 = open( f+".txt" , "r")
     archivo1.write(archivo2.read())
+    print(archivi2.read())
     archivo1.close()
     archivo2.close()
 	
-
+    
