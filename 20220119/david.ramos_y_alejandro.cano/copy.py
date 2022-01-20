bdfd4f80d4041f9ccf1b4747c9256692ef43434b
alejandrocano <alejandro.cano@iteso.mx>
2022-01-19 18:17:13 -0600

    copy

--- 20220119/david.ramos_y_alejandro.cano/copy.py
+++ 20220119/david.ramos_y_alejandro.cano/copy.py
@@ -1,11 +1,12 @@
 import os
 
-buscar = input("Ingresa el nombre del archivo, direccion/extension")
+buscar = input("Ingresa el nombre del archivo, direccion/extension: \n")
 
 if not os.path.exists(buscar):
     print("El archivo no existe")
 else:
 
-    archivo = open(buscar, "r")
-
-
+    archivo = buscar.replace(".txt", "-copia.txt")
+    new_name = "cp " + buscar + " " + archivo
+    os.system(new_name)
+    print("Archivo copia creado")
