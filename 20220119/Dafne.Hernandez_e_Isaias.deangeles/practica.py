e82530b4c80e48e6e6f2898fda19b2d57f30c2ec
Isayasa663 <isaias.deangeles@iteso.mx>
2022-01-20 16:04:54 -0600

    fin

--- 20220119/Dafne.Hernandez_e_Isaias.deangeles/practica.py
+++ 20220119/Dafne.Hernandez_e_Isaias.deangeles/practica.py
@@ -7,18 +7,18 @@ if os.path.exists(archivo):
     f = open(archivo, "r")
     contenido = f.read()  # se guarda en una variable
     f.close()
-    nuevo = ''
-    for n in range(len(archivo)):
-        if archivo[n] == ".":
-            if archivo[n+1] =="t":
-                if archivo[n+2] =="x":
-                    nuevo = archivo[:n] + '-Copia' + archivo[n:]
+    nuevo = archivo[:-4] + "-copia" + archivo[-4:]
+    #for n in range(len(archivo)):
+    #    if archivo[n] == ".":
+    #        if archivo[n+1] =="t":
+    #            if archivo[n+2] =="x":
+    #                nuevo = archivo[:n] + '-Copia' + archivo[n:]
     f = open(nuevo,"w")
     f.write(contenido) # se escribe contenido
     print("se copio")
     f.close()
 
-    f  = open("copia2.txt")
+    f  = open(nuevo)
 
     for x in f:
         print(x.strip())
