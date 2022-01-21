fa6cf9ba3a96d2edf0f98fd74dad2a295b3877d6
Antonio Josué Rodríguez Barrera <josue.rodriguez@iteso.mx>
2022-01-21 15:45:52 -0600

    avance

--- 20220121/juan.cardona_y_josue.rodriguez_y_alonso.nunez/practica.py
+++ 20220121/juan.cardona_y_josue.rodriguez_y_alonso.nunez/practica.py
@@ -14,5 +14,29 @@ f1.write(f.read())
 f1.close()
 f.close() 
 
+#---------------------------------------------
+informacion=open("/proc/cpuinfo")
+texto= str(informacion.read())
+posicion=texto.find("model name")
+#print(posicion)
+posicion2=texto.find("\nstepping")
+#print(posicion2)
+
+print("CPU INFO:")
+#---------Nombre del procesador--------
+for x in range(posicion,posicion2):
+     print(texto[x],end="")
+print()
+
+#---------Numero de procesadores--------
+processor=texto.count("processor")
+print("processors number:",processor)
+
+informacion.close()
+
+#Yo armé esto banda hasta ahorita solo jala el nombre del procesador y el número de procesadores chequen si jala en su compu 
+ 
+#----------------------------------------------
+
 #Hice la parte que copia el archivo 'cpuinfo', falta especificar el
 #contenido que especifica la practica.
