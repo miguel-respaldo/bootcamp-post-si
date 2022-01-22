63e5d976dbd1d258964e20284725746e474139ce
Isayasa663 <isaias.deangeles@iteso.mx>
2022-01-21 21:10:53 -0600

    fin

---
+++ 20220121/il723092_e_isaias.deangeles/practica.py
@@ -0,0 +1,17 @@
+
+import os 
+
+
+text = open("/proc/cpuinfo","r")
+lineas = text.readlines()
+
+
+for line in lineas:
+	if 'model name' in line:
+		print(f"El Modelo Del Procesador Es: {line[13:]}", end="")
+		break
+for line in lineas:
+	if 'cpu cores' in line:
+		print(f"Los Nucleos de su procesador son: {line[10:]}", end="")
+		break
+
