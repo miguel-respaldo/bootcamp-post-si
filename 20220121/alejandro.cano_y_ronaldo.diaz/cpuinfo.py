7293ea32b2028ad142e17d18627de7f664196af5
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-01-24 11:26:18 -0600

    Ultimo commit

--- 20220121/alejandro.cano_y_ronaldo.diaz/cpuinfo.py
+++ 20220121/alejandro.cano_y_ronaldo.diaz/cpuinfo.py
@@ -7,13 +7,12 @@
 """
 Esta practica es para obtener info del CPU
 """
-
 def main():
     """
     Se obtiene la informacion del archivo /proc/cpuinfo
     """
     print("Este programa obtiene informacion de los procesadores en tu maquina")
-   
+    print("Puedes consultar esa informacion en el archivo generado")
     cpu = open("/proc/cpuinfo", 'r') # Abrir para eer archivo
     #cpu_info = str(cpu.read())
     #print(cpu_info)
@@ -38,14 +37,15 @@ def main():
     print(cpu_cores)
     print(int(cpu_pfisicos)/int(cpu_plogicos))
     '''
-    
-    info = open("info_cpu.txt", "w") # para que reescriba si existe
-    info.write("\nEste archivo contiene la informacion de tu maquina \n")
+
+    # Crear el archivo para copiar la informacion
+    info = open("info_cpu.txt", "w")    # si existe se reescribe
+    info.write("\nEste archivo contiene la informacion de tu maquina: \n\n")
     info.write(cpu_modelo+"\n")
-    info.write(str(cpu_pfisicos)+"\n")
-    info.write(cpu_plogicos+"\n")
-    info.write(cpu_cores+"\n")
-    info.write(str(int(cpu_pfisicos)/int(cpu_plogicos))+"\n")
+    info.write("Numero de procesadores fisicos: "+str(cpu_pfisicos)+"\n")
+    info.write("Numero de procesadores logicos: "+cpu_plogicos+"\n")
+    info.write("Numero de cores: "+cpu_cores+"\n")
+    info.write("Hilos por core: "+str(int(cpu_pfisicos)/int(cpu_plogicos))+"\n")
     info.close()
 
 if __name__ == "__main__":
