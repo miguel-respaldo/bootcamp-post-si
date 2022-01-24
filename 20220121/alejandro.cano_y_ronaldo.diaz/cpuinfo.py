9b6c83c3fcf377859f5d3afecc04742dbe1816a3
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-01-24 09:29:56 -0600

    primer commit

---
+++ 20220121/alejandro.cano_y_ronaldo.diaz/cpuinfo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Esta practica es para obtener info del CPU
+"""
+
+def main():
+    """
+    Se obtiene la informacion del archivo /proc/cpuinfo
+    """
+    print("Este programa obtiene informacion de los procesadores en tu maquina")
+    
+
+if __name__ == "__main__":
+    main()
+
