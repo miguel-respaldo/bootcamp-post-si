3545d57e0d52bdcc26f079935ed3f997163913eb
Diego-GM <diegoe.garcia@iteso.mx>
2022-01-24 09:18:15 -0700

    segunda version

--- 20220121/diegoe.garcia_y_pedro.sanchez/cpuinf.py
+++ 20220121/diegoe.garcia_y_pedro.sanchez/cpuinf.py
@@ -7,18 +7,32 @@
 """
 script para obtener informacion del cpuinfo
 """
-import sys
 
 def main():
-    path="/proc/cpuinfo"
-    arch=open(path)
-    while(True):
-        linea = arch.readline()
-        print(linea)
-        if not linea:
-            break:
-    arch.close()
-
+    archivo = open("/proc/cpuinfo",'r') #Abre la informacion del sistema
+    #texto = archivo.read()
+    #print(texto)
+    
+    #Realiza la busqueda de las palabras que comiencen con lo que se necesita del archivo
+    for palabras in archivo: 
+        if palabras.startswith("model name"):
+            nombre_procesador=(palabras[palabras.find(":")+1:-1])
+            print("Nombre del procesador:"+nombre_procesador) 
+        if palabras.startswith("physical id"):
+            physical_id=(palabras[palabras.find(":")+1:-1])
+            print("Procesadores fisicos:"+physical_id)
+        if palabras.startswith("cpu cores"):
+            cpu_cores=(palabras[palabras.find(":")+1:-1])
+            print("Numero de cores:"+cpu_cores)
+        if palabras.startswith("stepping"):
+            stepping=(palabras[palabras.find(":")+1:-1])
+            print("Hilos por core:"+stepping)
+        if palabras.startswith("siblings"):
+            siblings=(palabras[palabras.find(":")+1:-1])
+            print("Procesadores logicos:"+siblings)
+            
+  
+    archivo.close()
 
 if __name__ == "__main__":
     main()
