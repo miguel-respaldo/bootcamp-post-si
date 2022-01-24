32b7aaa7352cb5032c83284d15c064d3c2ac84c9
Axel Rosillo Enciso <axel.rosillo@iteso.mx>
2022-01-23 21:13:13 -0600

    Practica CPU Info

--- 20220121/dafne.hernandez_y_axel.rosillo/cpu_info.py
+++ 20220121/dafne.hernandez_y_axel.rosillo/cpu_info.py
@@ -10,10 +10,29 @@ Practica: CPU info
 
 
 def main():
-    archivo = open('/proc/cpuinfo')
-    texto = archivo.read()
-    print(texto)
+    archivo = open("/proc/cpuinfo",'r') #Abre la informacion del sistema
+    #texto = archivo.read()
+    #print(texto)
     
+    #Realiza la busqueda de las palabras que comiencen con lo que se necesita del archivo
+    for palabras in archivo: 
+        if palabras.startswith("model name"):
+            nombre_procesador=(palabras[palabras.find(":")+1:-1])
+        if palabras.startswith("physical id"):
+            physical_id=(palabras[palabras.find(":")+1:-1])
+        if palabras.startswith("cpu cores"):
+            cpu_cores=(palabras[palabras.find(":")+1:-1])
+        if palabras.startswith("stepping"):
+            stepping=(palabras[palabras.find(":")+1:-1])
+        if palabras.startswith("siblings"):
+            siblings=(palabras[palabras.find(":")+1:-1])
+            
+    #Comienza a imprimir toda la informacion
+    print("Nombre del procesador:"+nombre_procesador) 
+    print("Procesadores fisicos:"+physical_id)
+    print("Numero de cores:"+cpu_cores)
+    print("Hilos por core:"+stepping)
+    print("Procesadores logicos:"+siblings)
     archivo.close()
 
 if __name__ == "__main__":
