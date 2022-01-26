16bb6bb2707e4115ae904ac3a0fd8e5d90832d32
Isayasa663 <isaias.deangeles@iteso.mx>
2022-01-26 02:02:57 -0600

    final

--- 20220121/il723092_e_isaias.deangeles/practica_variante.py
+++ 20220121/il723092_e_isaias.deangeles/practica_variante.py
@@ -2,15 +2,17 @@
 import os 
 
 
-text = open("/proc/cpuinfo","r")
+#text = open("/proc/cpuinfo","r")
+text = open("cpuinfo","r")
 lineas = text.readlines()
 
 
 for line in lineas:
+	n = line.index(":")
 	if 'model name' in line:
-		print(f"El Modelo Del Procesador Es: {line[13:]}", end="")
+		print(f"El Modelo Del Procesador Es: {line[n+2:]}", end="")
 	if 'siblings' in line:
-		print(f"El numero de Hilos Es: {line[11:]}", end="")	
+		print(f"El Numero De Procesadores Logicos Es: {line[n+2:]}", end="")	
 	if 'cpu cores' in line:
-		print(f"El numero de nucleos Es:{line[11:]}", end="")
+		print(f"El Numero De Nucleos Es:{line[n+2:]}", end="")
 		break
