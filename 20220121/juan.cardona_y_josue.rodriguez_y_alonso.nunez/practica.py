cec2ef9581abddd5498fa59f816a4cc2033829af
JosueCardona <cardonagarcia97@gmail.com>
2022-01-26 11:35:36 -0600

    modificacion

--- 20220121/juan.cardona_y_josue.rodriguez_y_alonso.nunez/practica.py
+++ 20220121/juan.cardona_y_josue.rodriguez_y_alonso.nunez/practica.py
@@ -9,7 +9,7 @@ Practica: Obtener informacion de un archivo y obtener informacion de el.
 """
 
 #f = open('/proc/cpuinfo', 'r') #abrimos cpuinfo en modo lectura
-f=open('/home/josuec/Documentos/Python/bootcamp-post-si/20220121/juan.cardona_y_josue.rodriguez_y_alonso.nunez/cpuinfo','r') 
+f=open('cpuinfo','r') 
 f1 = open('cpuinfo_copia.txt', 'w')
 f1.write(f.read())
 f1.close()
@@ -22,38 +22,42 @@ informacion=open("cpuinfo")
 texto= str(informacion.read())
 print("CPU INFO:")
 #---------Nombre del procesador------------------#
-posicion=texto.find("model name")
-posicion2=texto.find("\nstepping")
 
-for x in range(posicion,posicion2):
-     print(texto[x],end="")
-print()
+idx= int(texto.count("processor"))  #numero de iteracciones
+i = 0
+while(i < idx):
+    posicion=texto.find("model name")
+    posicion2=texto.find("\nstepping")
+    for x in range(posicion,posicion2):
+        print(texto[x],end="")
+        print()
 
 #---------Numero de procesadores lógicos---------#
-procesador=int(texto.count("processor"))
-print("logic processors number:",procesador)
+    procesador=int(texto.count("processor"))
+    print("logic processors number:",procesador)
 
 #---------CPU CORES------------------------------#
 
-posicion=texto.find("cpu cores")
-posicion2=texto.find("\napicid")
+    posicion=texto.find("cpu cores")
+    posicion2=texto.find("\napicid")
 
-for x in range(posicion,posicion2):
-     print(texto[x],end="")
-cores=int(texto[posicion2-1])
-print()
+    for x in range(posicion,posicion2):
+        print(texto[x],end="")
+        cores=int(texto[posicion2-1])
+    print()
 #---------NUMERO DE PROCESADORES FISICOS---------#
-posicion=texto.rfind("physical id")
-posicion2=texto.rfind("siblings")
+    posicion=texto.rfind("physical id")
+    posicion2=texto.rfind("siblings")
 #print(posicion)
 #print(posicion2)
-for x in range(posicion,posicion2):
-     print(texto[x],end="")
+    for x in range(posicion,posicion2):
+        print(texto[x],end="")
 #---------NUMERO DE HILOS------------------------#
 
-hilos=int(procesador/cores)
-print("Threads per core:",hilos)
-informacion.close()
+    hilos=int(procesador/cores)
+    print("Threads per core:",hilos)
+    informacion.close()
+    i += 1
 
 #Lo ejecute en mi computadora y todo funciona correctamente. -Alonso 
 
