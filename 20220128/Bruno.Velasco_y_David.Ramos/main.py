0b291051f118208c666788a47bba7082ebcc8cb8
Bruo Giancarlo Velasco Bravo <bruno.velasco@iteso.mx>
2022-01-28 11:07:31 -0600

    Ya quedo el ordenamiento con decoraciones

--- 20220128/Bruno.Velasco_y_David.Ramos/main.py
+++ 20220128/Bruno.Velasco_y_David.Ramos/main.py
@@ -5,17 +5,21 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 import datetime
-from statistics import variance
 import time
 import random
 
-def log(funcion):
+f = open("log.txt","a")
 
-    f = open("log.txt","a")
-    f.write("La función con argumentos: \n" + funcion() + " fue llamada  a las " + str(datetime.datetime.now()) + "\n")
-    f.close()
-    #res = funcion(*args, **kwargs)
-    #return res"
+def log(funcion):
+    def wrapper(args):
+        
+        
+        f.write("La función con argumentos: \n" + funcion(args) + " fue llamada  a las " + str(datetime.datetime.now()) + "\n")
+        
+        #res = funcion(*args, **kwargs)
+        #return res"
+        
+    return wrapper
 
 @log
 def burbuja(cantidad):
@@ -39,6 +43,41 @@ def burbuja(cantidad):
     variable += 'Esta es la lista ordenadita \n'
     variable += str(lista) + '\n'
     variable += 'Este tiempo dure paps: \n'
-    variable +=  str(time.time() - timestart) + '\n'
+    variable +=  str(time.time() - timestart) + '\n\n'
 
     return variable
+
+@log
+def ordenamientoSort(cantidad):
+    sutano = cantidad
+
+    lista = [random.randint(0,999) for x in range(sutano)]
+
+    timestart = time.time()
+    variable = 'lista desordenada es esta: \n'
+    variable += str(lista) + '\n'
+
+    lista.sort()
+            
+    variable += 'Esta es la lista ordenadita\n'
+    variable += str(lista) +'\n'
+    variable += 'Este tiempo dure paps: '
+    variable += str(time.time() - timestart) +'\n'
+    
+    return variable
+
+print("Que tipo de ordenamiento deseas")
+print("1) Burbuja")
+print("2) Sort")
+seleccion1 = int(input())
+
+seleccion2 = int(input("Cuantos numeros se van a generar? "))
+
+if (seleccion1 == 1):
+    burbuja(seleccion2)
+elif (seleccion1 == 2):
+    ordenamientoSort(seleccion2)
+else:
+    print("Invalido")
+
+f.close()
