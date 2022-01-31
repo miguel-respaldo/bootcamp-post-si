8be5c1363dc192486bc1eaefeb905247c04a0ca9
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-01-31 10:39:38 -0600

    Ordenamientos

--- 20220128/ronaldo.diaz_y_angel.ramos/practica.py
+++ 20220128/ronaldo.diaz_y_angel.ramos/practica.py
@@ -8,13 +8,29 @@
 Practica de Decoradores
 """
 
-def main():
-    """
-    Comentario de la función
-    """
-    print("Hola Mundo")
+import random
+
+# Genera matriz random
+matriz = [random.randint(1,100) for x in range(1000)]
 
 
-if __name__ == "__main__":
-    main()
+matriz.sort()               # Ordenamiento con funcion sort
 
+def bubbleSort(matriz):     # Ordenamiento con metodo burbuja
+    n = len(matriz)
+
+    # Se recorren todos los elementos
+    for i in range(n-1):
+
+        # Se recorren los elementos de dos en dos con el elemnto siguiente
+        for j in range(0, n-i-1):
+
+            # Realiza un cambio si el elemento siguiente es mayor
+            if matriz[j] > matriz[j + 1] :
+                matriz[j], matriz[j + 1] = matriz[j + 1], matriz[j]
+
+bubbleSort(matriz)
+
+print ("La matriz arreglado: ")
+for i in range(len(matriz)):
+    print (matriz[i],end=" ")
