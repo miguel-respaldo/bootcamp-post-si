7105015df1626f69ce0935a38123937d2eda6200
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-01-31 11:46:17 -0600

    Primer version jalando

--- 20220128/ronaldo.diaz_y_angel.ramos/practica.py
+++ 20220128/ronaldo.diaz_y_angel.ramos/practica.py
@@ -8,14 +8,32 @@
 Practica de Decoradores
 """
 import random
+import datetime
+import time
 
 # Genera matriz random
 matriz = [random.randint(1,100) for x in range(1000)]
 
+def log(funcion):
+    inicio = datetime.datetime.now()
+    def wrapper(*args, **kwargs):
+        elementos = len(args[0])
+        logs = open("log.txt","a")
+        logs.write("Funcion con "+str(elementos)+" elementos ")
+        logs.write("inicio a las "+str(inicio)+"\n")
+        #logs.close()
+        
+        res = funcion(*args, **kwargs)
+        return res
+    return wrapper
+
+
+@log
 def sort(*args):        # Ordenamiento con la funcion sort
     matriz.sort()
     print(matriz)
 
+@log
 def bubble(*args):     # Ordenamiento con metodo burbuja
     n = len(matriz)
 
@@ -29,7 +47,7 @@ def bubble(*args):     # Ordenamiento con metodo burbuja
             if matriz[j] > matriz[j + 1] :
                 matriz[j], matriz[j + 1] = matriz[j + 1], matriz[j]
 
-    print ("La matriz arreglado: ")
+    print ("La matriz arreglada: ")
     for i in range(len(matriz)):
         print (matriz[i],end=" ")
 
