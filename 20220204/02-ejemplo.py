798193bcadb26a7f8fa3ad03f87f3ce9013c625d
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-02-04 08:37:15 -0600

    numpy

---
+++ 20220204/02-ejemplo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+
+arr = np.array([1,2,3,4,5])
+
+print(arr)
+print(np.__version__)
