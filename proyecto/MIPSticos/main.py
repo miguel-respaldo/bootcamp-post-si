af12595cc1ea72944e28d69e5b46dd8b0309e714
Javier-Vl <jvelazquez.lozano@iteso.mx>
2022-02-03 21:57:25 -0600

    identificando inst tipo i

--- proyecto/MIPSticos/main.py
+++ proyecto/MIPSticos/main.py
@@ -5,9 +5,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from mnemonicos import mnemonicos_dict_I,mnemonicos_dict_J
-
-
+from diccionarios import mnemonicos_dict_I,mnemonicos_dict_J,registers
 
 
 
@@ -17,6 +15,7 @@ def read_assembly(asm_name) :  # retorna una lista con las lineas del archivo si
     return lines 
 
 def identify_instr(line): # retornar el tipo de instruccion y su opcode en decimal
+    line = line.strip() # quitar los espacios alprincipio y al final
     mnemonic = line.split(" ")
     inst = mnemonic[0] # Se obtiene la instruccion
     if inst in mnemonicos_dict_I.keys():
@@ -26,17 +25,29 @@ def identify_instr(line): # retornar el tipo de instruccion y su opcode en decim
 
         return("J",mnemonicos_dict_J[inst])
     else:
-  
         return("R",0)
     
-
+def generate_type_I(line): # GENERA TIPOS I
+    line=line.strip() # quitar los espacios alprincipio y al final
+    line = line.split(" ") #se convierte en una lista de 2 elementos 
     
+    setInst = identify_instr(line[0]) # tupla coon type y opcode decimal
     
+    opc = bin(setInst[1])[2:].zfill(6) # opcode etsitosament
+    
+    trinity = line[1].split(",")
+  
+    rs = bin(registers[trinity[0]])[2:].zfill(5)
+    rt = bin(registers[trinity[1]])[2:].zfill(5)
+    immte = bin(int(trinity[2]))[2:].zfill(16)
 
+        
     
 lines_of_file= read_assembly("caliz.asm")
 
-caliz = identify_instr(lines_of_file[4]) # addi
+instrType = identify_instr(lines_of_file[0]) # addi
 
-print(caliz)
-print(lines_of_file[4])
+print(instrType)
+print(lines_of_file[0])
+generate_type_I(lines_of_file[0])
+
