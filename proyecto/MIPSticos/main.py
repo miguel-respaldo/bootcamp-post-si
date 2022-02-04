f5a1c34fc656340cb01fa2dc55723902666f4e70
Javier-Vl <jvelazquez.lozano@iteso.mx>
2022-02-03 20:38:26 -0600

    decodificando texto

--- proyecto/MIPSticos/main.py
+++ proyecto/MIPSticos/main.py
@@ -5,5 +5,22 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from mnemonicos import mnemonicos_dict
 
-hola
+def read_assembly(asm_name) -> list:  # retorna una lista con las lineas del archivo sin salto de linea
+    with open(asm_name) as f: 
+        lines = [line.rstrip('\n') for line in f] # quita el salto de linea    
+    return lines 
+
+def identify_instr(line):
+    mnemonic = line.split(" ")
+    return mnemonic[0]
+
+    
+    
+
+    
+lines_of_file= read_assembly("caliz.asm")
+caliz = identify_instr(lines_of_file[0])
+print(mnemonicos_dict[caliz])
+print(lines_of_file[0])
