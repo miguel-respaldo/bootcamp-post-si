5f676a1a636045a8635f401afcd3102a3344ba5b
Javier-Vl <jvelazquez.lozano@iteso.mx>
2022-02-08 00:02:53 -0600

    identificando inst tipo j

--- proyecto/MIPSticos/main.py
+++ proyecto/MIPSticos/main.py
@@ -6,6 +6,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
+from pickletools import opcodes
 import re
 
 
@@ -16,11 +17,11 @@ labels_list =[]#se almacenarar etiquetas Y dir en mem correspondiente
 
 def read_assembly(asm_name) :  # retorna una lista con las lineas del archivo sin salto de linea
     with open(asm_name) as f: 
-        lines = [re.sub(r"[\n\t\s]*", "", line) for line in f] # quita el salto de linea 
+        lines = [re.sub(r"[\n\t]*", "", line) for line in f] # quita el salto de linea 
     return lines 
 
 def identify_labels(file_lines):
-    PROGRAM_COUNTER = 400000 # por cada linea aumeNta 4
+    PROGRAM_COUNTER = 4194304 # por cada linea aumeNta 4
     for inst in file_lines:
         inst = inst.split(":") # si encuentra : etiqueta
         if(len(inst)==2):#contiene etiqute
@@ -46,12 +47,20 @@ def identify_instr(line): # retornar el tipo de instruccion y su opcode en decim
         return("R",0)
     
 def generate_type_J(line):
-    line =line.lstrip()
-    print(line)
-    
+    line =line.strip()
     line = line.split(" ")
-    print(line)
-    pass
+    opc = mnemonicos_dict_J[line[0]] #obteniendo key
+    opc = bin(opc)[2:].zfill(6) # opcode etsitosament
+    
+    for item in labels_list: #recorrer la lista que contine las etiquetas con la dir
+       if line[1] in item:
+           value = item[1] # la addres se encuntra en la pos 1 de la tupla           
+           value =value >>3 # corriemiento para que coincida la dir
+           address =bin(value)[2:].zfill(25)# eliminar prefijo 0b rellenar de 0 para completar
+           #print(address)
+    
+    return opc+address
+    
     
 def generate_type_I(line): # GENERA TIPOS I
     line=line.strip() # quitar los espacios alprincipio y al final
@@ -88,5 +97,6 @@ def generate_type_R(line):
 lines_of_file= read_assembly("caliz.asm") # aqui se obtiene el archivo como una lista
 identify_labels(lines_of_file)
 
-generate_type_J(lines_of_file[13])
+
+print(generate_type_J(lines_of_file[13]))
 
