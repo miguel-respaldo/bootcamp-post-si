efc5ec86011f3bceedb68c5b77572847fce7b56d
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-02-10 15:06:49 -0600

    correccion memonicos

--- proyecto/parejatoxica/memonicos.py
+++ proyecto/parejatoxica/memonicos.py
@@ -1,6 +1,6 @@
 
 def instruction_decode(instruccion):
-    tipoR = {'add' : 0 , 'and' : 2, 'jr' : 0xa, 'or' : 0xc, 'sll' : 0xe, 'slr' : 0xf } #Tipo R 
+    tipoR = {'add' : 0 , 'and' : 2, 'jr' : 0xa, 'or' : 0xc, 'sll' : 0xe, 'srl' : 0xf } #Tipo R 
     tipoI = {'addi' : 0x1 , 'andi' : 0x3, 'lb' : 0xb, 'sb' : 0xd } #Tipo I
     tipoB = {'beq' : 0x4 , 'bne' : 0x5 } #Tipo Branch
     tipoJ = {'j' : 0x6 , 'jal' : 0x7 } #Tipo J
