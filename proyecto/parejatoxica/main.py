c876d131218f109b2c675d96fad62d6034c4b470
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-02-09 19:04:42 -0600

    prueba

--- proyecto/parejatoxica/main.py
+++ proyecto/parejatoxica/main.py
@@ -10,16 +10,34 @@ Proyecto
 import os
 
 def main():
-
-    programa = input("Introduzca el nombre del programa MIPS: ")
-
-    if os.path.exists(programa):
-        f = open(programa, 'r')
+    
+    #--------------------- LEE EL ARCHIVO Y TIPO DE SALIDA --------------------
+    archivo = input("Introduzca el nombre del programa MIPS: ")
+    while not os.path.exists(archivo):
+        archivo = input("El archivo no existe, introduzca otro nombre: ")
+    
+    print("\nPuedes obtener el archivo de salida en:\n(1)-Texto\n(2)-Binario")
+    salida = int(input("Indica el tipo de salida del programa: "))
+    while not (salida==1 or salida==2):
+        salida = int(input("Tipo de salida invalido, vuelve a intentar: "))
         
-        f.close()
-    else:
-        print("El archivo no existe")
-
     print("Ya estas dentro de proyecto")
+
+    
+    #---------------------- GUARDAR CADA INSTRUCCION --------------------------
+    programa = open(archivo,'r')
+    contador = 0
+
+    for linea in programa:
+        lista = linea.split(",")
+        contador = 0
+        for valor in lista:
+            print("V{}".format(contador),"=",valor.strip(),end=("/"))
+            contador += 1
+        print()
+    
+    programa.close
+
+
 if __name__ == "__main__":
     main()
