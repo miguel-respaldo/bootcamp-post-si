f3f88eabd31bc59d96432fa10d4665d3f5dea1d7
ronaldodiaz0 <ronaldo.diaz@iteso.mx>
2022-02-09 22:56:15 -0600

    Arriba la carne asada

--- proyecto/parejatoxica/main.py
+++ proyecto/parejatoxica/main.py
@@ -15,28 +15,55 @@ def main():
     archivo = input("Introduzca el nombre del programa MIPS: ")
     while not os.path.exists(archivo):
         archivo = input("El archivo no existe, introduzca otro nombre: ")
-    
+   
+    '''
     print("\nPuedes obtener el archivo de salida en:\n(1)-Texto\n(2)-Binario")
     salida = int(input("Indica el tipo de salida del programa: "))
     while not (salida==1 or salida==2):
         salida = int(input("Tipo de salida invalido, vuelve a intentar: "))
-        
-    print("Ya estas dentro de proyecto")
+    
+    '''
+    print("\nYa estas dentro de proyecto\n")
 
     
     #---------------------- GUARDAR CADA INSTRUCCION --------------------------
     programa = open(archivo,'r')
     contador = 0
+    lineas_codigo = 0
+
+    # Crear el archivo para copiar la informacion
+    code_output = open("output.txt", "w")    # si existe se reescribe
+    code_output.write("\nEste archivo contiene la informacion de tu maquina: \n\n")
 
     for linea in programa:
         lista = linea.split(",")
         contador = 0
+        lineas_codigo += 1
+        
         for valor in lista:
-            print("V{}".format(contador),"=",valor.strip(),end=("/"))
-            contador += 1
-        print()
+
+            if (valor.find(":") != -1):     # busca si hay etiquetas
+                salto = valor.split(":")    # crea un arreglo y separa 
+                etiqueta = salto[0]         # el primer elemento es etiqueta
+                valor = salto[1]            # el segundo elemento es instruccion
+                valor = valor.strip()       # quita espacios
+                print(etiqueta,end=(" / "))
+                #print("v{}".format(contador),"=",valor,end="; ")
+
+                #mandar llamar funcion
+                contador += 1               # aumenta el contador del valor
+            else:
+                valor = valor.strip()       # quita espacios
+                #print("v{}".format(contador),"=",valor,end="; ")
+
+                #mandar llamar funcion
+                contador += 1               # aumenta el contador del valor
+
+        print("Esta es la linea:",lineas_codigo)
+    print("\nEl codigo tiene:",lineas_codigo)
     
-    programa.close
+    programa.close()
+
 
 
 if __name__ == "__main__":
