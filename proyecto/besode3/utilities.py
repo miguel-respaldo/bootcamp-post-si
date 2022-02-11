6546604af342f5eafee6d3d6c6f52877213b53b8
Bruno Giancarlo Velasco Bravo <brunogiancarlo.velasco@gmail.com>
2022-02-10 22:00:00 -0600

    Libreria acutlizada

--- proyecto/besode3/utilities.py
+++ proyecto/besode3/utilities.py
@@ -37,6 +37,7 @@ def getType(nemonic):
     if nemonic in rType: return 'R'
     if nemonic in iType: return 'I'
     if nemonic in jType: return 'J'
+    else: 'X'
     return None
 
 # R[rd] = R[rs] + R[rt]
@@ -82,3 +83,82 @@ def instructionJ(nemonic, address):
     out += address
     
     return out
+
+def convertArgumentsR(nemonic, args):
+    
+    if nemonic == "jr":
+        # Las instrucciones "jr" solo usan rs
+        rs = int(args[0][1: ])
+        rt, rd = 0, 0
+    elif nemonic == 'sll' or nemonic == 'srl':
+        # Solo instrucciones 'sll' y 'slr' cambia el orden de los argumentos de 'rs, rt, rd'
+        rd = int(args[0][1: ])
+        rt = int(args[1][1: ])
+        rs = int(args[2][1: ])
+    else:
+        # Para el resto de las instrucciones
+        rd = int(args[0][1: ])
+        rs = int(args[1][1: ])
+        rt = int(args[2][1: ])
+    
+    return rs, rt, rd
+
+def convertArgumentsI(nemonic, args):
+    # Convertir en numeros los args
+    for i in range(len(args)):
+        if args[i].isnumeric(): # Para numeros positivos
+            args[i] = int(args[i]) 
+        elif args[i].startswith('-') and args[i][1:].isnumeric(): # Para numeros negativos
+            args[i] = int(args[i][1:]) 
+            args[i] *= -1
+            args[i] = args[i] + (1 << 8)
+        elif args[i].startswith('x'): # Para numeros los registros
+            args[i] = int(args[i][1 :])
+        elif args[i].startswith('0x'): # Para numeros hexadecimales
+            args[i] = int(args[i], 16)
+        elif args[i].startswith('0b'): # Para numeros binarios
+            args[i] = int(args[i], 2)
+    
+    if nemonic != 'beq' and nemonic != 'bne':
+        # Para cualquier otra que no sea 'beq' y 'bne'
+        rt = args[0]
+        rs = args[1]
+        imm = args[2]
+        
+        #codigoMaquina = instructionI(nemonico, rt, rs, imm)
+    
+    if nemonic == 'lb' or nemonic == 'sb':
+        # 'lb' y 'sb' tiene un orden distinto al guardar los args
+        rt = args[0]
+        imm = args[1]
+        rs = args[2]
+        
+        #codigoMaquina = instructionI(nemonico, rt, rs, imm)
+    else:
+        # Aqui van  a entrar las instrucciones 'beq' y 'bne' 
+        # pero no se genera codigo maquina para generarlo despues cuando encuentre a donde salta
+        rt = args[0]
+        rs = args[1]
+        imm = args[2]
+
+    return rt, rs, imm
+
+def findJumps(inputList):
+    # Las funciones de salto como 'j', 'jal', 'beq' y 'bnq' necesitan saber a que linea saltar
+    # buscará la instruccion con la etiquta a donde salta para igualar el PC a donde debe saltar
+    for i in inputList:
+        if i.address != None:
+            for j in inputList:
+                if i.address == j.tag: 
+                    # Aquí solo entran las instrucciones de tipo J
+                    i.address = j.pc
+                    i.machineCode = instructionJ(i.nemonic, i.address)
+                    break
+                    
+
+                
+                if i.imm == j.tag: 
+                    # Aqui entran los bne y beq
+                    i.imm = j.pc - i.pc
+                    i.machineCode = instructionI(i.nemonic, i.rs, i.rt, i.imm)
+                    break
