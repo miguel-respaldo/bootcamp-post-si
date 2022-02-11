3f01dd996305c3da18edf61d9ca9989a9888b1bb
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2022-02-10 20:42:17 -0600

    el principal pa

---
+++ proyecto/discipulos_de_PizaGod/main_mips.py
@@ -0,0 +1,21 @@
+# Converts MIPS instructions into binary and hex
+import os
+import sys
+import argparse #para parsear
+
+from dic_mnemonicos import instr_decode # converts the instruction part of a line of MIPS code
+from registerlist import reg_decode # converts the register and immediate parts of the MIPS code
+
+#########
+PC=1  #añadido  
+ETIQUETAS = [] #añadido
+DIR_ETIQUETAS=[] #añadido
+##########
+
+def readFile(filen): #Function that reads a file 
+    f = open(filen, "r") #read permissions 
+    content = f.read().split("\n") #splits input after line jump
+    f.close()
+    return content
+
+# the main conversion function
