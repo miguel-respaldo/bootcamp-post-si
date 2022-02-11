1453c01db632c4abcdf9c885a52556d2862b09b2
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2022-02-10 20:33:04 -0600

    Cambios a los mnemonicos

--- proyecto/discipulos_de_PizaGod/dic_nmemonicos.py
+++ proyecto/discipulos_de_PizaGod/dic_nmemonicos.py
@@ -1,4 +1,6 @@
 def instr_decode(instr):
     tipoR = {'add' : 0 , 'and' : 2, 'jr' : 0xa, 'or' : 0xc, 'sll' : 0xe, 'slr' : 0xf }
     tipoI = {'addi' : 0x1 , 'andi' : 0x3, 'lb' : 0xb, 'sb' : 0xd }
-
+    tipoB = {'beq' : 0x4 , 'bne' : 0x5 } #Tipo Branch
+    tipoJ = {'j' : 0x6 , 'jal' : 0x7 }
+   
