07aa29a37ca79f92742f358526cb650a2e116fd8
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2022-02-11 18:47:30 -0600

    inicio de la conversion

--- proyecto/discipulos_de_PizaGod/convert.py
+++ proyecto/discipulos_de_PizaGod/convert.py
@@ -1,1 +1,8 @@
-#hi
+# the main conversion function
+from dic_mnemonicos import instr_decode # converts the instruction part of a line of MIPS code
+from dic_register import reg_decode # converts the register and immediate parts of the MIPS code
+
+def converting(code):
+    code = code.replace(" ", "")
+    args = code.split(" ") #dividir despues de cada espacio
+    instruction = args[0]
