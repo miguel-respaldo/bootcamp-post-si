dc9b9078eb4a114c4b06f3480ff26af23e36bc2a
Isayas663 <isaias.deangeles@iteso.mx>
2022-02-11 20:11:58 -0600

    Modification

--- proyecto/discipulos_de_PizaGod/convert.py
+++ proyecto/discipulos_de_PizaGod/convert.py
@@ -4,5 +4,13 @@ from dic_register import reg_decode # converts the register and immediate parts 
 
 def converting(code):
     code = code.replace(" ", "")
+    code = code.replace(",", " ") 
+    code = code.replace("  ", " ") 
+    code = code.replace("   ", "") 
+    code = code.replace(":", " ") 
     args = code.split(" ") #dividir despues de cada espacio
     instruction = args[0]
+    codes = instr_decode(instruction) 
+    func_type = codes[0]   
+    reg_values = reg_decode(func_type, instruction, args[1:]) 
+ 
