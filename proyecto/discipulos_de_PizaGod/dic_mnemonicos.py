f23c05a668b09c45ba53a78002be50735667d3c0
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2022-02-11 18:27:54 -0600

    otros cambios en dic_mnemonicos

--- proyecto/discipulos_de_PizaGod/dic_mnemonicos.py
+++ proyecto/discipulos_de_PizaGod/dic_mnemonicos.py
@@ -3,21 +3,21 @@ def instr_decode(instr):
     tipoI = {'addi' : 0x1 , 'andi' : 0x3, 'lb' : 0xb, 'sb' : 0xd }
     tipoB = {'beq' : 0x4 , 'bne' : 0x5 } #Tipo Branch
     tipoJ = {'j' : 0x6 , 'jal' : 0x7 }
-   
-if instr in tipoR:
+
+   if instr in tipoR:
         opcode = tipoR[instr]
         func_type = "r"
     elif instr in tipoI:
         opcode = tipoI[instr]
-        func_type = "i"    
+        func_type = "i"
     elif instr in tipoB:
         opcode = tipoB[instr]
-        func_type = "b"    
+        func_type = "b"
     elif instr in tipoJ:
         opcode = tipoJ[instr]
         func_type = "j"
     else:
         func_type = None
         opcode = None
-    
+
     return [func_type, opcode]
