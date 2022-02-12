0cdb53abc041301d610437b9b5591f0d67a9d27c
Isayas663 <isaias.deangeles@iteso.mx>
2022-02-11 21:08:27 -0600

    raton

--- proyecto/discipulos_de_PizaGod/dic_register.py
+++ proyecto/discipulos_de_PizaGod/dic_register.py
@@ -1,1 +1,6 @@
-#diccionarios 
+git #diccionarios 
+
+registers = {"x0" : 0,"x1" : 1,"x2" : 2,"x3" : 3,"x4" : 4,"x5" : 5,"x6" : 6,"x7" : 7
+}
+labels = { "MAIN" : 1,"INC" : 4,"DEC" : 9,"FUNC" : 8,"EXIT" : 15
+}
