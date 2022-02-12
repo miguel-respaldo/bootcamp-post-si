bf4e039dcceb659edbb936b7b2706a5a3e924983
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2022-02-11 21:18:59 -0600

    funcion tipo r

--- proyecto/discipulos_de_PizaGod/dic_register.py
+++ proyecto/discipulos_de_PizaGod/dic_register.py
@@ -1,6 +1,17 @@
-git #diccionarios 
+#diccionario para los registros
 
-registers = {"x0" : 0,"x1" : 1,"x2" : 2,"x3" : 3,"x4" : 4,"x5" : 5,"x6" : 6,"x7" : 7
+registers = {"x0" : 0, "x1" : 1, "x2" : 2, "x3" : 3, "x4" : 4, "x5" : 5, "x6" : 6, "x7" : 7
 }
-labels = { "MAIN" : 1,"INC" : 4,"DEC" : 9,"FUNC" : 8,"EXIT" : 15
+labels = { "MAIN" : 1, "INC" : 4, "DEC" : 9, "FUNC" : 8, "EXIT" : 15
 }
+
+if func_type == "r": 
+        #special case "sll" | "srl"
+        if (instr == "sll") or (instr == "srl"): 
+            try:
+                if (regs[0] < regs[1]):
+                    return [registers[regs[1]], registers[regs[1]], registers[regs[1]], 0] 
+                else:
+                    return [registers[regs[0]], registers[regs[1]], registers[regs[0]], 0] 
+            except:
+                return 0 #incompleto
