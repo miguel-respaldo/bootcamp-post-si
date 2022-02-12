d1112281531221a300a76705f3c8fbdd016dfd95
Dafne Angelica Hernandez Vazquez <mcflurrycerezita@gmail.com>
2022-02-11 20:18:17 -0600

    func_type

--- proyecto/discipulos_de_PizaGod/convert.py
+++ proyecto/discipulos_de_PizaGod/convert.py
@@ -2,15 +2,23 @@
 from dic_mnemonicos import instr_decode # converts the instruction part of a line of MIPS code
 from dic_register import reg_decode # converts the register and immediate parts of the MIPS code
 
-def converting(code):
-    code = code.replace(" ", "")
-    code = code.replace(",", " ") 
-    code = code.replace("  ", " ") 
-    code = code.replace("   ", "") 
-    code = code.replace(":", " ") 
-    args = code.split(" ") #dividir despues de cada espacio
+def converting(instr):
+    instr = instr.replace(" ", "")
+    instr = instr.replace(",", " ") 
+    instr = instr.replace("  ", " ") 
+    instr = instr.replace("   ", "") 
+    instr = instr.replace(":", " ") 
+    args = instr.split(" ") #dividir despues de cada espacio
     instruction = args[0]
     codes = instr_decode(instruction) 
     func_type = codes[0]   
     reg_values = reg_decode(func_type, instruction, args[1:]) 
  
+    #execution for b-type functions
+    if func_type == "b":
+        opcode = '{0:04b}'.format(codes[1])
+        rs = '{0:03b}'.format(reg_values[0])
+        rt = '{0:03b}'.format(reg_values[1])
+        label = '{0:08b}'.format(reg_values[2])
+        b = opcode+rs+rt+label #binary
+        print(b)
